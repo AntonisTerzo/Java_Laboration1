251e5a831509a5b01eb8e2006caa62826763f947
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-09-27 14:52:03 +0200

    created the quantity discount

--- src/main/java/Decorators/QuantityDiscount.java
+++ src/main/java/Decorators/QuantityDiscount.java
@@ -1,4 +1,29 @@
 package Decorators;
 
-public class QuantityDiscount {
+import Component.Discount;
+import Entities.Product;
+
+public class QuantityDiscount extends BaseDiscount{
+    private static final double QUANTITY_DISCOUNT = 10.0;
+    private static final int MINIMUM_QUANTITY = 5;
+    private static final String QUANTITY_DESCRIPTION = "Quantity discount(at least " + MINIMUM_QUANTITY + " products): " + QUANTITY_DISCOUNT + "kr per product";
+
+    public QuantityDiscount(Discount discount) {
+        super(discount);
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.quantity() * QUANTITY_DISCOUNT;
+    }
+
+    @Override
+    protected String getDiscountDescription(Product product) {
+        return QUANTITY_DESCRIPTION;
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return product.quantity() >= MINIMUM_QUANTITY;
+    }
 }
--- src/main/java/Entities/CustomerCart.java
+++
@@ -1,6 +0,0 @@
-package Entities;
-
-import java.util.List;
-
-public record CustomerCart(String name, int age, List<Product> products) {
-}
