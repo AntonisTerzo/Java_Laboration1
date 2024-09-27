936fa51469fe95b9c68a2025a5ec727b2818224e
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-09-27 14:33:25 +0200

    created the milk discount

--- src/main/java/Decorators/MilkDiscount.java
+++ src/main/java/Decorators/MilkDiscount.java
@@ -1,4 +1,30 @@
 package Decorators;
 
-public class MilkDiscount {
+import Component.Discount;
+import Entities.Product;
+
+public class MilkDiscount extends BaseDiscount{
+    private static final double MILK_DISCOUNT = 0.05; 
+    private static final String MILK_DESCRIPTION = "Milk discount: 5%";
+
+
+    public MilkDiscount(Discount discount) {
+        super(discount);
+    }
+
+    @Override
+    protected double calculateDiscount(Product product) {
+        return product.price() * MILK_DISCOUNT;
+    }
+
+    @Override
+    protected String getDiscountDescription(Product product) {
+        return MILK_DESCRIPTION;
+    }
+
+    @Override
+    protected boolean isApplicable(Product product) {
+        return product.name().toLowerCase().contains("milk");
+    }
+    
 }
